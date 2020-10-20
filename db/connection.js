dbe7c9936b898bc58e6629aa30a1b373c8dc105b
Gourav <gore.auluck@gmail.com>
2020-10-19 21:07:37 -0700

    updates

--- db/connection.js
+++ db/connection.js
@@ -15,7 +15,7 @@ mongoose.connect(MONGODBURI, config);
 const db = mongoose.connection;
 
 db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
-db.on('connected', () => console.log('mongo connected: ', mongoURI));
+db.on('connected', () => console.log('mongo connected: ', MONGODBURI));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
 module.exports = mongoose;
